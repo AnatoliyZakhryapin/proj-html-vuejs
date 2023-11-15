268d83e63669eb344d6b90b8a5a7f3aacc952951
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-15 17:31:46 +0100

    ADD to PageHeader "navMenu" db

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,4 +1,39 @@
-<script setup>
+<script>
+
+export default {
+    data() {
+        return {
+            navMenu: {
+                links: [
+                    {
+                        text: "Homes",
+                        href: "#"
+                    },
+                    {
+                        text: "Pages",
+                        href: "#"
+                    },
+                    {
+                        text: "Blog",
+                        href: "#"
+                    },
+                    {
+                        text: "Shop",
+                        href: "#"
+                    },
+                    {
+                        text: "Events",
+                        href: "#"
+                    },
+                    {
+                        text: "Elements",
+                        href: "#"
+                    },
+                ]
+            }
+        }
+    }
+}
 </script>
 
 <template>
