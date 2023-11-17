d6548e1674d6e89f858ec0a9948a4964b1153e65
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-17 13:20:19 +0100

    ADD SectionLatestNew

---
+++ src/components/SectionLatestNews.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<template>
+    <section class="latest-news">
+        latest-news
+    </section>
+</template>
+
+<style lang="scss" lscoped>
+    @use './style/partial/varibils' as*;
+    @use './style/partial/mixins' as*;
+</style>
