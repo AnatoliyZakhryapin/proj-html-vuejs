5c0a1f59aa3712b12dc3f785e0ab7b31f59f7fee
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-17 12:13:45 +0100

    ADD SectionCreativeLeader

---
+++ src/components/SectionCreativeLeader.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+       Creative
+    </div>
+</template>
+
+<style lang="scss" lscoped>
+    @use './style/partial/varibils' as*;
+    @use './style/partial/mixins' as*;
+</style>
