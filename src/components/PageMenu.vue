c421fab69963a508abcaa466ccaaab24965a2e74
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-19 21:32:23 +0100

    ADD underline to header menu

--- src/components/PageMenu.vue
+++ src/components/PageMenu.vue
@@ -1,6 +1,11 @@
 <script>
 
 export default {
+    data() {
+        return {
+            activeIndex: Number,
+        }
+    },
     props: {
         title: {
             type: String,
@@ -11,6 +16,16 @@ export default {
             required: false
         }, 
         links: Array,
+    },
+    methods: {
+        isActive(index) {
+            if(index === this.activeIndex) {
+                return true
+            }
+        },
+        getActive(index) {
+            this.activeIndex = index;
+        }
     }
 }
 </script>
@@ -18,7 +33,12 @@ export default {
 <template>
     <h3 v-if="title"> {{ title }} </h3>
     <ul class="menu">
-        <li class="menu-item" v-for="link in links">
+        <li 
+            class="menu-item" 
+            :class="{'active': isActive(index)}"
+            v-for="(link, index) in links"
+        
+        >
             <a  
                 v-if="link.icon"
                 class="icon"  
@@ -30,6 +50,7 @@ export default {
                 v-if="link.text" 
                 class="text" 
                 :href="link.href"
+                @click="getActive(index)"
             >
                 <span v-if="arrow" class="arrow">
                     <!-- &#10230; -->
