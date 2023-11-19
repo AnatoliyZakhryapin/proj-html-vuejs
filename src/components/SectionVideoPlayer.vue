46ed7e57d811dd91d29cafccb54ef8a1f85cad1e
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-19 23:34:34 +0100

    ADD loader to SectionVideoPlayer

--- src/components/SectionVideoPlayer.vue
+++ src/components/SectionVideoPlayer.vue
@@ -2,20 +2,36 @@
     export default {
         data() {
             return {
-                
+              playIsActive: false,  
             }
+        },
+        methods: {
+            changePlayStatus() {
+                this.playIsActive = !this.playIsActive
+            },
         }
     }
 </script>
 
 <template>
-    <section class="video-player">
+    <section class="video-player" 
+        @click="changePlayStatus()"
+    >
         <div class="containre-fluid">
             <figure>
                 <img src="/img/h1-img-04.jpg">
             </figure>
             <div class="play-icon">
-                <font-awesome-icon icon="fa-solid fa-play" />
+                <font-awesome-icon 
+                    icon="fa-solid fa-play" 
+                    v-if="!playIsActive"
+                    @click.stop="changePlayStatus()"
+                />
+                <img 
+                    :class="{'active' : playIsActive}"
+                    class="loader" 
+                    src="/img/ajax-loader.gif"
+                >
             </div>
         </div>
     </section>
@@ -35,7 +51,14 @@
             svg {
                 color: $f-white-2;
                 font-size: 30px;  
-            } 
+            }
+            .loader {
+                width: 30px;
+                display: none;
+                &.active {
+                    display: block;
+                }
+            }
         }
     }
 
