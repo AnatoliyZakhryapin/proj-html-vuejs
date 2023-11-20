a274018990a8e6fedd5708ce7204ad2e06b699a0
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-20 09:08:35 +0100

    ADD section to header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -69,56 +69,58 @@ export default {
 </script>
 
 <template>
-    <header class="page-header">
-        <div class="container-xxl">
-            <div class="row">
-                <!-- LOGO -->
-                <div class="col">
-                    <figure>
-                        <img class="logo" src="/img/logo-img-01.png" alt="">
-                    </figure>
-                </div>
-                <!-- NAVIGATION BAR -->
-                <div class="col">
-                    <div class="nav-bar">
-                        <!-- NAV-MENU -->
-                        <div class="nav-menu">
-                            <PageMenu
-                                :links="navMenu.links"
-                                :arrow="navMenu.arrow"
-                            />
-                        </div>
-                        <!-- INPUT SEARCH -->
-                        <div 
-                            class="nav-element search-bar" 
-                            @click.stop="getActiveInput()"
-                            :class="{'active' : isActiveInput}"
-                        >   
-                            <a class="search-bar__icon input" >
-                                <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
-                            </a> 
-                            <input 
-                                type="text" 
-                                placeholder="Search"
-                                v-model="searchText"
-                                ref="name"  
-                            > 
-                            <a class="search-bar__xmark" 
-                                :class="{'active' : searchText.length > 0}"
-                                @click.stop="clearSearchText()"
-                            >
-                                <font-awesome-icon icon="fa-solid fa-xmark" />
-                            </a>    
-                        </div>
-                        <!-- SECONDARY MENU -->
-                        <div class="menu-secondary">
-                            <font-awesome-icon icon="fa-solid fa-list" />
+    <section class="header">
+        <header class="page-header">
+            <div class="container-xxl">
+                <div class="row">
+                    <!-- LOGO -->
+                    <div class="col">
+                        <figure>
+                            <img class="logo" src="/img/logo-img-01.png" alt="">
+                        </figure>
+                    </div>
+                    <!-- NAVIGATION BAR -->
+                    <div class="col">
+                        <div class="nav-bar">
+                            <!-- NAV-MENU -->
+                            <div class="nav-menu">
+                                <PageMenu
+                                    :links="navMenu.links"
+                                    :arrow="navMenu.arrow"
+                                />
+                            </div>
+                            <!-- INPUT SEARCH -->
+                            <div 
+                                class="nav-element search-bar" 
+                                @click.stop="getActiveInput()"
+                                :class="{'active' : isActiveInput}"
+                            >   
+                                <a class="search-bar__icon input" >
+                                    <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                                </a> 
+                                <input 
+                                    type="text" 
+                                    placeholder="Search"
+                                    v-model="searchText"
+                                    ref="name"  
+                                > 
+                                <a class="search-bar__xmark" 
+                                    :class="{'active' : searchText.length > 0}"
+                                    @click.stop="clearSearchText()"
+                                >
+                                    <font-awesome-icon icon="fa-solid fa-xmark" />
+                                </a>    
+                            </div>
+                            <!-- SECONDARY MENU -->
+                            <div class="menu-secondary">
+                                <font-awesome-icon icon="fa-solid fa-list" />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
+    </section>
 </template>
 
 <style lang="scss" scoped>
